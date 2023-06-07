d5555c2be88a4babac0a12dd42f753d89e65f44e
jonatan mansilla <yoniii-11@live.com.ar>
2023-06-06 22:22:24 -0300

    subida de retoque solo falta su carga

--- js/funcion.js
+++ js/funcion.js
@@ -1,7 +1,7 @@
 function sendText() {
-    var textInput = document.getElementById('textInput').value;
-    window.location.href = 'another-page.html?text=' + encodeURIComponent(textInput);
-  }
+  var textInput = document.getElementById('textInput').value;
+  window.location.href = 'another-page.html?text=' + encodeURIComponent(textInput);
+}
 /** SECTOR DE LOS AÑOS AUTOMATICA PARA EL FOOTER **/
 const año = document.getElementById("copi");
 año.innerHTML = new Date().getFullYear();
@@ -9,16 +9,22 @@ año.innerHTML = new Date().getFullYear();
 
 
 
-
-
-btn = document.getElementById("btn"),
-    on = document.querySelectorAll(".on")
+let btn = document.getElementById("btn"),
+  on = document.querySelectorAll(".on")
 function intercambiarClases(array, clase) {
   array.forEach(element => {
-      element.classList.toggle(clase);
+    element.classList.toggle(clase);
   })
-}
+}/* 
+let nombre = document.getElementById("first_name").value;
+let apellido = document.getElementsById("last_name").value;
+let nombres = document.querySelector("");
+nombres.innerHTML = "<h4>nombre  +apellido</h4>"; */
+
+
+
 btn.addEventListener("click", (e) => {
   e.preventDefault()
   intercambiarClases(on, "none");
+
 })
