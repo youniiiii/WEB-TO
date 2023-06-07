22a0385566521a23e90af7b775305e5ab765901c
jonatan mansilla <yoniii-11@live.com.ar>
2023-06-06 21:53:29 -0300

    subidas de  la utlima actualizacion

--- js/funcion.js
+++ js/funcion.js
@@ -5,3 +5,20 @@ function sendText() {
 /** SECTOR DE LOS AÑOS AUTOMATICA PARA EL FOOTER **/
 const año = document.getElementById("copi");
 año.innerHTML = new Date().getFullYear();
+
+
+
+
+
+
+btn = document.getElementById("btn"),
+    on = document.querySelectorAll(".on")
+function intercambiarClases(array, clase) {
+  array.forEach(element => {
+      element.classList.toggle(clase);
+  })
+}
+btn.addEventListener("click", (e) => {
+  e.preventDefault()
+  intercambiarClases(on, "none");
+})
